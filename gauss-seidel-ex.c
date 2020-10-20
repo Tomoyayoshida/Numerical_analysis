5d2e0b4f0044feda81e2bacbe6b7330953cf84e6
tomoya yoshida <kakesyou13@gmail.com>
2020-10-21 02:07:53 +0900

    Add second program

--- gauss-seidel-ex.c
+++ gauss-seidel-ex.c
@@ -15,19 +15,33 @@ double get_x3(double x1,double x2)
 	return((13.0-2.0*x1-x2)/3.0);
 }
 
+double fabs(double x)//絶対値表現を行うための関数
+{
+	if(x > 0)
+		return x;
+	else
+		return -x;
+}
+
 int main()
 {
-	double x1,x2,x3;
+	double a=141,b=141,c=141,x1,x2,x3;//変化前の変数値を代入するための変数を作成(a,b,c)
 	int i;
 
 	x1 = x2 = x3 = 1.0;
-	for(i=0;i<20;i++)
+	for(i=0;i<20;i++,a=x1,b=x2,c=x3)
 	{
 		x1 = get_x1(x2,x3);
 		x2 = get_x2(x1,x3);
 		x3 = get_x3(x1,x2);
 		printf("[i=%d]:x1 = %.10f :x2 =%.10f :x3 = %.10f\n",i+1,x1,x2,x3);
+		if((fabs(a)-fabs(x1)) < 0.00000001 && (fabs(b)-fabs(x2)) < 0.00000001 &&(fabs(c)-fabs(x3)) < 0.00000001)//任意の値で収束するようにif文を設計する
+		{
+			printf("convergence\n");//収束すると繰り返しが終了する
+			break;
+		}
 	}
+
 	return 0;
 }
 
